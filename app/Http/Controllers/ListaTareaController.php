9cab5fc3604b743d661b7395b8a5de187be7dbd6
caremperador <filtrosperu@gmail.com>
2024-05-27 16:48:31 -0500

    barra de progreso

--- app/Http/Controllers/ListaTareaController.php
+++ app/Http/Controllers/ListaTareaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\ListaTarea;
 use Illuminate\Http\Request;
+use App\Enums\EstadoTareaEnum;
 
 class ListaTareaController extends Controller
 {
@@ -29,20 +30,35 @@ class ListaTareaController extends Controller
         ListaTarea::create($request->all());
         return redirect()->route('listas_tareas.index');
     }
-
     public function show(ListaTarea $listasTarea)
     {
         $listasTarea->load('user', 'tareas');
-        return view('listas_tareas.show', compact('listasTarea'));
+        $totalTareas = $listasTarea->tareas->count();
+        $tareasCompletadas = $listasTarea->tareas->where('estado', EstadoTareaEnum::COMPLETADA)->count();
+        $progreso = $totalTareas > 0 ? ($tareasCompletadas / $totalTareas) * 100 : 0;
+        $colorbarra = '';
+
+        if ($progreso < 25) {
+            $colorbarra = 'bg-red-500';
+        } elseif ($progreso < 50) {
+            $colorbarra = 'bg-orange-500';
+        } elseif ($progreso < 75) {
+            $colorbarra = 'bg-yellow-500';
+        } else {
+            $colorbarra = 'bg-green-500';
+        }
+
+        return view('listas_tareas.show', compact('listasTarea', 'progreso', 'colorbarra'));
     }
-    
+
+
 
     public function edit(ListaTarea $listasTarea)
     {
         $usuarios = User::all();
         return view('listas_tareas.edit', compact('listasTarea', 'usuarios'));
     }
-    
+
 
     public function update(Request $request, ListaTarea $listas_tarea)
     {
@@ -50,12 +66,12 @@ class ListaTareaController extends Controller
             'user_id' => 'required|exists:users,id',
             'nombre' => 'required|string|max:255',
         ]);
-    
+
         $listas_tarea->update($request->all());
-    
+
         return redirect()->route('listas_tareas.index')->with('success', 'Lista de tareas actualizada exitosamente.');
     }
-    
+
 
     public function destroy(ListaTarea $listas_tarea)
     {
