8bafa325cdb07cbb44d8e57383974e0d9181a89f
caremperador <filtrosperu@gmail.com>
2024-08-07 18:46:16 -0500

    home page foto de perfil y rutas protegidas

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,27 @@ use App\Http\Controllers\CasoController;
 use App\Http\Controllers\ListaRequisitoController;
 
 
-Route::resource('casos', CasoController::class);
+/* Route::resource('casos', CasoController::class);
 Route::resource('tareas', TareaController::class);
-Route::resource('listas_requisitos', ListaRequisitoController::class);
+Route::resource('listas_requisitos', ListaRequisitoController::class); */
 
+Route::middleware(['role:admin|asistente'])->group(function () {
+    Route::resource('casos', CasoController::class)->except(['show']);
+    Route::resource('tareas', TareaController::class);
+    Route::resource('requisitos', RequisitoController::class)->except(['show']);
+    Route::resource('listas_requisitos', ListaRequisitoController::class)->except(['show']);
+});
 
-Route::get('requisitos/search', [RequisitoController::class, 'search'])->name('requisitos.search');
+// Ruta abierta para cualquier usuario registrado
+Route::middleware(['auth'])->group(function () {
+    Route::get('casos/{caso}', [CasoController::class, 'show'])->name('casos.show');
+    Route::get('requisitos/{requisito}', [RequisitoController::class, 'show'])->name('requisitos.show');
+    Route::get('listas_requisitos/{listas_requisito}', [ListaRequisitoController::class, 'show'])->name('listas_requisitos.show');
+});
+
 Route::get('requisitos/create/{lista_requisito?}', [RequisitoController::class, 'create'])->name('requisitos.create');
-Route::resource('requisitos', RequisitoController::class)->except(['create']);
+Route::get('requisitos/search', [RequisitoController::class, 'search'])->name('requisitos.search');
+
 
 
 
@@ -26,7 +39,7 @@ Route::resource('requisitos', RequisitoController::class)->except(['create']);
 }); */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::middleware([
